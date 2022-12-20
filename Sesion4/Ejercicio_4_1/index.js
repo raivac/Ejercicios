e7cd7285453e3db73bf4ab74c42e669779cb7252
Raúl <raulivars1997@gmail.com>
2022-12-20 11:12:40 +0100

    sesion 4

---
+++ Sesion4/Ejercicio_4_1/index.js
@@ -0,0 +1,19 @@
+const os = require('os');
+const express = require('express');
+
+let app = express();
+
+app.listen(8080);
+
+//http://localhost:8080/fecha
+app.get('/fecha',(req,res) => {
+    const fecha = new Date();
+    res.send(`Hoy es ${fecha.toLocaleDateString()}`);
+});
+
+
+//http://localhost:8080/usuario
+app.get('/usuario',(req,res) => {
+    const usuario = os.hostname();
+    res.send(`Usuario conectado ${usuario}`);
+});
